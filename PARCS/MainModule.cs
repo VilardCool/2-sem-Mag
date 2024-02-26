b34bc829e030d9982ef8f48513998fa931795d0f
VilardCool <61537368+VilardCool@users.noreply.github.com>
2024-02-26 11:52:37 +0200

    Add files via upload

---
+++ PARCS/MainModule.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using Parcs;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CentroidLinkage
+{
+    class MainModule : IModule
+    {
+        public static void Main(string[] args)
+        {
+
+            var job = new Job();
+            job.AddFile(Assembly.GetExecutingAssembly().Location);
+            if (job == null)
+            {
+                Console.WriteLine("File doesn't exist");
+                return;
+            }
+
+            (new MainModule()).Run(new ModuleInfo(job, null));
+            Console.ReadKey();
+        }
+        
+        static double EucDistance(List<double> a, List<double> b)
+        {
+            return Math.Sqrt(Math.Pow(a[0] - b[0], 2) + Math.Pow(a[1] - b[1], 2) + Math.Pow(a[2] - b[2], 2));
+        }
+        
+        static List<int> FindClosestCentroids(List<List<double>> img, List<List<double>> colors)
+        {
+            int m = img.Count;
+            List<int> centroids = new List<int>();
+
+            for (int i = 0; i < m; i++)
+            {
+                List<double> distances = new List<double>();
+
+                for (int j = 0; j < colors.Count; j++)
+                {
+                    distances.Add(EucDistance(img[i], colors[j]));
+                }
+
+                centroids.Add(distances.IndexOf(distances.Min()));
+            }
+
+            return centroids;
+        }
+        
+        static List<List<int>> CentroidLinkage(List<List<double>> img, List<List<double>> colors)
+        {
+            List<int> idx = FindClosestCentroids(img, colors);
+
+            List<List<int>> recolored_img = new List<List<int>>();
+
+            for (int i = 0; i < idx.Count; i++)
+            {
+                for (int j = 0; j < colors.Count; j++)
+                {
+                    if (idx[i] == j)
+                    {
+                        List<int> color = new List<int>();
+                        for (int k = 0; k < 3; k++)
+                        {
+                            color.Add((int)(colors[j][k] * 255));
+                        }
+                        recolored_img.Add(color);
+                        break;
+                    }
+                }
+            }
+
+            return recolored_img;
+        }
+
+        public void Run(ModuleInfo info, CancellationToken token = default(CancellationToken))
+        {
+            int amount = 16;
+
+            Console.Write("Number of colors: ");
+            amount = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("File name: ");
+            string file = Console.ReadLine();
+
+            Bitmap newImage = (Bitmap)Image.FromFile("input/" + file + ".png");
+
+            Console.Write("Color divide: ");
+            int divided = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Sort: ");
+            int sort = Convert.ToInt32(Console.ReadLine());
+
+            List<List<Tuple<double, double[]>>> dist = new List<List<Tuple<double, double[]>>>();
+
+            List<Tuple<double, double[]>> distR = new List<Tuple<double, double[]>>();
+            List<Tuple<double, double[]>> distG = new List<Tuple<double, double[]>>();
+            List<Tuple<double, double[]>> distB = new List<Tuple<double, double[]>>();
+
+            List<Tuple<double, double[]>> distOrig = new List<Tuple<double, double[]>>();
+
+            List<List<double>> img = new List<List<double>>();
+
+            for (int i = 0; i < newImage.Height; i++)
+            {
+                for (int j = 0; j < newImage.Width; j++)
+                {
+                    double r = newImage.GetPixel(j, i).R / 255.0;
+                    double g = newImage.GetPixel(j, i).G / 255.0;
+                    double b = newImage.GetPixel(j, i).B / 255.0;
+                    double d = Math.Sqrt(r * r + g * g + b * b);
+
+                    img.Add(new List<double>() { r, g, b });
+
+                    double[] color = new double[3] { r, g, b };
+
+                    Tuple<double, double[]> dis = new Tuple<double, double[]>(d, color);
+
+                    if (divided == 1)
+                    {
+                        if (r >= g && r >= b) distR.Add(dis);
+                        else if (g >= r && g >= b) distG.Add(dis);
+                        else if (b >= r && b >= g) distB.Add(dis);
+                    }
+                    else distOrig.Add(dis);
+                }
+            }
+
+            DateTime time = DateTime.Now;
+            Console.WriteLine("Waiting for result...");
+
+            if (divided == 1)
+            {
+                dist.Add(distR);
+                dist.Add(distG);
+                dist.Add(distB);
+            }
+            else dist.Add(distOrig);
+
+            if (sort == 1)
+                for (int k = 0; k < dist.Count; k++)
+                {
+                    var newDist = dist[k].OrderBy(c => c.Item1).ThenBy(b => b.Item2[0]).ThenBy(c => c.Item2[1]).ToList();
+                    dist[k] = newDist;
+                }
+
+            List<List<double>> colors = new List<List<double>>();
+
+            for (int i = 0; i < dist.Count; i++)
+                for (int j = 0; j < dist[i].Count; j++)
+                {
+                    List<double> color = new List<double>();
+                    for (int k = 0; k < 3; k++) color.Add(dist[i][j].Item2[k]);
+
+                    if (sort == 1 && colors.Count > 0
+                        && colors[colors.Count - 1][0] == color[0]
+                        && colors[colors.Count - 1][1] == color[1]
+                        && colors[colors.Count - 1][2] == color[2]) continue;
+
+                    colors.Add(color);
+                }
+
+            const int pointsNum = 4;
+            var points = new IPoint[pointsNum];
+            var channels = new IChannel[pointsNum];
+            for (int i = 0; i < pointsNum; ++i)
+            {
+                points[i] = info.CreatePoint();
+                channels[i] = points[i].CreateChannel();
+                points[i].ExecuteClass("CentroidLinkage.Module");
+            }
+
+            List<List<int>> res = new List<List<int>>();
+            List<List<double>> result = new List<List<double>>();
+
+            int part = colors.Count / pointsNum;
+            int lastPart = part + colors.Count % pointsNum;
+            int amountP = amount / pointsNum;
+            int lastAmount = amountP + amount % pointsNum;
+
+            for (int j = 0; j < pointsNum; j++)
+            {
+                if (j == pointsNum - 1)
+                {
+                    channels[j].WriteData(lastAmount);
+                    channels[j].WriteObject(colors.GetRange(part * j, lastPart));
+                }
+                else
+                {
+                    channels[j].WriteData(amountP);
+                    channels[j].WriteObject(colors.GetRange(part * j, part));
+                }
+            }
+
+            for (int j = 0; j < pointsNum; j++)
+            {
+                List<List<double>> resu = (List<List<double>>)channels[j].ReadObject();
+                for (int k = 0; k < resu.Count; k++) result.Add(resu[k]);
+            }
+
+            res = CentroidLinkage(img, result);
+
+            Console.WriteLine("Result: {0}s", Math.Round((DateTime.Now - time).TotalSeconds, 3));
+
+            var bitmap = new Bitmap(newImage.Width, newImage.Height);
+
+            for (var y = 0; y < bitmap.Height; y++)
+            {
+                for (var x = 0; x < bitmap.Width; x++)
+                {
+                    Color color = Color.FromArgb(res[y * bitmap.Width + x][0], res[y * bitmap.Width + x][1], res[y * bitmap.Width + x][2]);
+                    bitmap.SetPixel(x, y, color);
+                }
+            }
+
+            string output = "output/" + bitmap.Width + "x" + bitmap.Height + "_" + amount + "_" + divided + sort + ".png";
+
+            bitmap.Save(output);
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ PARCS/Module.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using Parcs;
+
+namespace CentroidLinkage
+{
+    public class Module : IModule
+    {
+        static double EucDistance(List<double> a, List<double> b)
+        {
+            return Math.Sqrt(Math.Pow(a[0] - b[0], 2) + Math.Pow(a[1] - b[1], 2) + Math.Pow(a[2] - b[2], 2));
+        }
+
+        static List<int> GetMin(List<List<double>> mat)
+        {
+            double min = double.MaxValue;
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < mat.Count; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (mat[i][j] < min)
+                    {
+                        min = mat[i][j];
+                        a = Math.Min(i, j);
+                        b = Math.Max(i, j);
+                    }
+                }
+            }
+
+            return new List<int>() { a, b };
+        }
+
+
+        static void ChangeCentroid(ref List<List<double>> mat, ref List<List<double>> colors, int a, int b)
+        {
+            List<double> centroid = new List<double>() { 0, 0, 0 };
+
+            for (int i = 0; i < 3; i++)
+            {
+                centroid[i] = (colors[a][i] + colors[b][i]) / 2.0;
+            }
+
+            for (int i = a + 1; i < colors.Count; i++)
+            {
+                if (i > b) mat[i].RemoveAt(b);
+                mat[i].RemoveAt(a);
+            }
+
+            mat.RemoveAt(b);
+            mat.RemoveAt(a);
+            colors.RemoveAt(b);
+            colors.RemoveAt(a);
+
+            colors.Add(centroid);
+
+            List<double> row = new List<double>();
+
+            for (int i = 0; i < colors.Count; i++)
+            {
+                row.Add(EucDistance(colors[i], centroid));
+            }
+            mat.Add(row);
+        }
+
+        static List<int> FindClosestCentroids(List<List<double>> img, List<List<double>> colors)
+        {
+            int m = img.Count;
+            List<int> centroids = new List<int>();
+
+            for (int i = 0; i < m; i++)
+            {
+                List<double> distances = new List<double>();
+
+                for (int j = 0; j < colors.Count; j++)
+                {
+                    distances.Add(EucDistance(img[i], colors[j]));
+                }
+
+                centroids.Add(distances.IndexOf(distances.Min()));
+            }
+
+            return centroids;
+        }
+
+        static List<List<int>> CentroidLinkage(List<List<double>> img, List<List<double>> colors)
+        {
+            List<int> idx = FindClosestCentroids(img, colors);
+
+            List<List<int>> recolored_img = new List<List<int>>();
+
+            for (int i = 0; i < idx.Count; i++)
+            {
+                for (int j = 0; j < colors.Count; j++)
+                {
+                    if (idx[i] == j)
+                    {
+                        List<int> color = new List<int>();
+                        for (int k = 0; k < 3; k++)
+                        {
+                            color.Add((int)(colors[j][k] * 255));
+                        }
+                        recolored_img.Add(color);
+                        break;
+                    }
+                }
+            }
+
+            return recolored_img;
+        }
+
+        private static List<List<double>> Linkage(int amount, List<List<double>> colors)
+        {
+            List<List<double>> mat = new List<List<double>>();
+
+            for (int i = 0; i < colors.Count; i++)
+            {
+                List<double> row = new List<double>();
+
+                for (int j = 0; j < i; j++)
+                {
+                    row.Add(EucDistance(colors[i], colors[j]));
+                }
+
+                mat.Add(row);
+            }
+
+            while (mat.Count > amount)
+            {
+                List<int> ab = GetMin(mat);
+                int fir = ab[0];
+                int sec = ab[1];
+
+                ChangeCentroid(ref mat, ref colors, fir, sec);
+            }
+
+            return colors;
+        }
+
+        static List<List<double>> Reduce(int amountFin, List<List<double>> colors)
+        {
+            List<List<double>> result = new List<List<double>>();
+            int amount = amountFin;
+            int box = amount + amount / 2 + 1;
+            /*
+            if (amount * colors.Count / box <= amountFin * box)
+            {
+                amount = amountFin;
+                box = amount + amount / 2 + 1;
+            }
+            */
+            for (int i = 0; i <= colors.Count / box; i++)
+            {
+                int part = box;
+                if (colors.Count - box * i < box)
+                {
+                    part = colors.Count - box * i;
+                }
+
+                List<List<double>> resu = Linkage(amount, colors.GetRange(box*i, part));
+                for (int k = 0; k < resu.Count; k++) result.Add(resu[k]);
+
+            }
+
+            if (result.Count > amountFin) result = Reduce(amountFin, result);
+
+            return result;
+        }
+
+        public void Run(ModuleInfo info, CancellationToken token = default(CancellationToken))
+        {
+            while (true)
+            {
+                int amount = info.Parent.ReadInt();
+
+                List<List<double>> colors = (List<List<double>>)info.Parent.ReadObject();
+
+                List<List<double>> result = Reduce(amount, colors);
+
+                info.Parent.WriteObject(result);
+
+            }
+        }
+    }
+}
